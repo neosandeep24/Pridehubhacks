037e001480fea994c38aee58b42a60e7db133fad
NeoSandeep <neosandeep24@gmail.com>
2023-06-19 12:52:27 +0530

    fixing errors

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,8 +1,21 @@
+import { motion } from "framer-motion";
 import "../styles/Loader.css";
 
 const Loader = () => {
   return (
     <div className="loader-container">
+      <motion.div
+        className="loader"
+        animate={{
+          rotate: [0, 360],
+          scale: [1, 0.8, 1],
+        }}
+        transition={{
+          duration: 1.5,
+          repeat: Infinity,
+          ease: "easeInOut",
+        }}
+      ></motion.div>
     </div>
   );
 };
